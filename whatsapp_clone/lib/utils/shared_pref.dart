b3f56d9391ea3b5100031d794f95e34bf272867d
Oussama <markad.oussama@gmail.com>
2024-04-14 17:32:02 +0100

    Delete old clone

--- whatsapp_clone/lib/utils/shared_pref.dart
+++
@@ -1,9 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class SharedPref {
-  static late SharedPreferences instance;
-
-  static Future<void> init() async {
-    instance = await SharedPreferences.getInstance();
-  }
-}
